2b753c569520c00ccd79aabc59b8388f885ac358
hwhitehead <hwhitehead25@gmail.com>
2026-01-07 16:16:14 +0000

    updated location info

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,27 +1,37 @@
 var addressPoints = [
   [
-    "Institute of Astronomy, Cambridge, UK",
+    "Institute of Astronomy<br />University of Cambridge, UK",
     52.21412,
     0.09325
   ],
   [
-    "Berkeley CA, USA",
-    37.8708393,
-    -122.2728638
+    "Department of Astrophysics<br /> University of Oxford, UK",
+    51.75953,
+    -1.25947
   ],
   [
-    "London, UK",
-    51.5073219,
-    -0.1276473
+    "Institute for Particle Physics and Astrophysics<br /> University of Zurich, Switzerland",
+    47.39793,
+    8.55050
   ],
   [
-    "San Francisco, California",
-    37.7792808,
-    -122.4192362
+    "Center for Astrophysics<br /> Harvard University, USA",
+    42.38148,
+    -71.12809
   ],
   [
-    "Los Angeles, CA",
-    34.0543942,
-    -118.2439408
+    "Department of Astrophysical Sciences<br /> Princeton University, USA",
+    40.34661,
+    -74.65166
+  ],
+  [
+    "Research Center for the Early Universe<br /> University of Tokyo, Japan",
+    35.71335,
+    139.76313
+  ],
+  [
+    "Niels Bohr International Academy<br /> University of Copenhagen, Denmark",
+    55.69682,
+    12.57154
   ]
 ];
